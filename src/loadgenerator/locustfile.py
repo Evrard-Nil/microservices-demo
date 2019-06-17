a71c8cc1bcfd9981fb319d2708194e7b731b8203
Daillet <38186198+Evrard-Nil@users.noreply.github.com>
2019-06-17 09:54:54 +0200

    Update locustfile.py

--- src/loadgenerator/locustfile.py
+++ src/loadgenerator/locustfile.py
@@ -159,12 +159,9 @@ class UserBehavior(TaskSet):
     def on_start(self):
         index(self)
 
-    tasks = {index: 1,
-        setCurrency: 2,
-        browseProduct: 10,
-        addToCart: 2,
-        viewCart: 3,
-        checkout: 1}
+    tasks = {
+        browseProduct: 100
+}
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
